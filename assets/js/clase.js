88f2187d327d62d5a7129bbe26c735df44b2c1b0
Ricardo Parra <ricardo.prxr16@gmail.com>
2023-02-04 17:42:14 -0400

    módulo clase modificado

--- assets/js/clase.js
+++ assets/js/clase.js
@@ -1,10 +1,26 @@
 $(document).ready(function(){
 	rellenar();
 	let mostrar;
+	function rellenar(){
+		$.ajax({
+			type: "post",
+			url: "",
+			dataType: "json",
+			data: {mostrar: "labs" },
+			success(list){
+				mostrar = $('#tabla').DataTable({
+					responsive: true,
+					data: list
+				})
+			}
+		})
+	}
+
+
 	$("#clase").keyup(()=> {  validarNombre($("#clase"),$("#error"), "Error de clase,") });
 	let vclase
 	$("#enviar").click((e)=>{
-
+		e.preventDefault();
 		vclase = validarNombre($("#clase"),$("#error") , "Error de clase,");
 
 		$.ajax({
@@ -25,51 +41,37 @@ $(document).ready(function(){
 		})
 	})
 
-	function rellenar(){
-		$.ajax({
-			type: "post",
-			url: "",
-			dataType: "json",
-			data: {mostrar: "labs" },
-			success(list){
-				mostrar = $('#tabla').DataTable({
-					responsive: true,
-					data: list
-				})
-			}
-		})
-	}
 
 	let id
 	$(document).on('click', '.borrar', function() {
-    	id = this.id;
-    });
-    	$('#borrar').click(()=>{
-    		$.ajax({
-    			type : 'post',
-    			url : '',
-    			dataType: 'json',
-    			data : {borrar : 'asd', id},
-    			success(data){
-    				mostrar.destroy();
-    				$('#cerrar').click();
-    				rellenar();
-    				Toast.fire({ icon: 'error', title: 'Clase eliminada' })
-    			}
-    		})
-    	})
+		id = this.id;
+	});
+	$('#borrar').click(()=>{
+		$.ajax({
+			type : 'post',
+			url : '',
+			dataType: 'json',
+			data : {borrar : 'asd', id},
+			success(data){
+				mostrar.destroy();
+				$('#cerrar').click();
+				rellenar();
+				Toast.fire({ icon: 'error', title: 'Clase eliminada' })
+			}
+		})
+	})
 
 
-    /* --- EDITAR --- */
+	/* --- EDITAR --- */
 	let idedit;
     // SELECCIONA ITEM
     $(document).on('click', '.editar', function() {
         idedit = this.id; // se obtiene el id del botón, previamente le puse de id el codigo en rellenar()
        	// RELLENA LOS INPUTS
-       		$.ajax({
-       			method: "post",
-       			url: "",
-       			dataType: "json",
+       	$.ajax({
+       		method: "post",
+       		url: "",
+       		dataType: "json",
 		        data: {item: "lol", idedit}, // id : id
 		        success(data){
 		        	$("#claseEdit").val(data[0].des_clase);
@@ -77,23 +79,23 @@ $(document).ready(function(){
 
 		    })
 
-	});
+       });
 
-	$("#claseEdit").keyup(()=> {  validarNombre($("#claseEdit"),$("#error2"), "Error de clase,") });
-	let eclase
-	$("#enviarEdit").click((e)=>{
+    $("#claseEdit").keyup(()=> {  validarNombre($("#claseEdit"),$("#error2"), "Error de clase,") });
+    let eclase
+    $("#enviarEdit").click((e)=>{
+    	e.preventDefault();
+    	eclase = validarNombre($("#claseEdit"),$("#error2") , "Error de clase,");
 
-		eclase = validarNombre($("#claseEdit"),$("#error2") , "Error de clase,");
-
-		$.ajax({
-			type: "post",
-			url: '',
-			dataType: 'json',
-			data: {
-				claseEdit: $("#claseEdit").val(),idedit
-			},
-			success(data){
-				if (eclase) {
+    	$.ajax({
+    		type: "post",
+    		url: '',
+    		dataType: 'json',
+    		data: {
+    			claseEdit: $("#claseEdit").val(),idedit
+    		},
+    		success(data){
+    			if (eclase) {
 					mostrar.destroy(); // VACÍA LA DATATABLE
 					rellenar();  // FUNCIÓN PARA RELLENAR DATATABLE
 				  	$('#closeEdit').click(); // CERRAR EL MODAL
@@ -101,7 +103,7 @@ $(document).ready(function(){
 				}
 			}
 		})
-	})
+    })
     
 
 
